e86919cca76977043ce8616bfa84851263bb66ec
David Mills <dave@1morepx.com>
2019-05-27 16:13:30 -0400

    splitting date string and returning the year

--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 # Talking Calendar
 
 We will be given a date as a string (not a Date object). The date will always be formatted as `YYYY/MM/DD`. We will have to parse the given string and produce a human readable date.
+
+## Tasks
+
+- Break down input into individual parts
+- Year stays the same
+- Month is converted to English name
+- Day has a suffix appended to it
--- talkingCalendar.js
+++ talkingCalendar.js
@@ -1,7 +1,17 @@
 let talkingCalendar = function(date) {
   // Your code here
+  var dateComponents = date.split("/");
+
+  var year = dateComponents[0];
+  var monthString = dateComponents[1];
+  var dayString = dateComponents[2];
+
+  return year;
 };
 
+// December 2nd, 2017
 console.log(talkingCalendar("2017/12/02"));
+// November 11th, 2007
 console.log(talkingCalendar("2007/11/11"));
+// August 24th, 1987
 console.log(talkingCalendar("1987/08/24"));
